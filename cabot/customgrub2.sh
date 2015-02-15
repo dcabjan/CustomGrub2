5d2c39556a934d8f0ee2c8ce75fa0b3b96154a2c
dcabjan <davc.984@gmail.com>
2015-02-15 01:05:59 +0100

    Gestión de contraseñas
    - Menu principal
    - Menu Gestion de contraseñas
    - Crear contraseña
    - Eliminar contraseña

---
+++ cabot/customgrub2.sh
@@ -0,0 +1,14 @@
+#carga la libreria de funciones
+. ./lib_grub.sh
+#Garcia
+#Garcia
+#Cabot
+#ventana del menú principal
+zenityMenuPrincipal
+#zenityCrearContra
+#echo si
+#Cabot
+#Dios
+#Dios
+#Catalan
+#Catalan
